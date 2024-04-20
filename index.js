3ab5944a77eb5c3b26033956b0e488dc32461eb7
Mohammad Aftab <160458810+AfTabaMiRanI@users.noreply.github.com>
2024-04-20 09:46:43 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+// import the "inquirer" module, which is a command line interface for Node.js
+import inquirer from "inquirer";
+import chalk from "chalk";
+// Declare a constant 'answer' and assign it to the result of inquirer.prompt functon
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: chalk.green `Enter Your Sentence to count the words:`
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// Print the array of words to the console.
+console.log(words);
+// print the word count of the sentence to the console
+console.log(chalk.blue `Your sentnece word count is: ${words.length}`);
+console.log(chalk.yellow `****Thank You****`);
